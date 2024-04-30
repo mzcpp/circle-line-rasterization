60e7b5cec5b785b60b4ee6f284f240e5aee31343
Mario <33102086+mzcpp@users.noreply.github.com>
2024-04-30 16:54:22 +0200

    Add files via upload

---
+++ include_example/include/Game.hpp
@@ -0,0 +1,42 @@
+#ifndef GAME_HPP
+#define GAME_HPP
+
+#include "CircleTexture.hpp"
+
+#include <SDL2/SDL.h>
+
+#include <memory>
+
+typedef int NumType;
+
+class Game
+{
+private:
+	bool initialized_;
+	bool running_;
+
+	SDL_Point mouse_pos_;
+	std::unique_ptr<CircleTexture<NumType>> circle_texture_;
+
+	SDL_Window* window_;
+	SDL_Renderer* renderer_;
+
+public:
+	Game();
+
+	~Game();
+
+	bool Initialize();
+
+	void Finalize();
+
+	void Run();
+
+	void HandleEvents();
+	
+	void Tick();
+	
+	void Render();
+};
+
+#endif
