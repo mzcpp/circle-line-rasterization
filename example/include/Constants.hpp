0f4b0aeef10780f6a0e074a9f12846a78f8f591c
Mario <33102086+mzcpp@users.noreply.github.com>
2024-04-30 16:55:24 +0200

    Add files via upload

---
+++ example/include/Constants.hpp
@@ -0,0 +1,11 @@
+#ifndef CONSTANTS_HPP
+#define CONSTANTS_HPP
+
+namespace constants
+{
+	inline constexpr char game_title[] = "Circle Texture Example"; 
+	inline constexpr int screen_width = 1024;
+	inline constexpr int screen_height = 1024;
+} // namespace constants
+
+#endif
---
+++ example/src/Game.cpp
@@ -0,0 +1,165 @@
+#include "Game.hpp"
+#include "Constants.hpp"
+#include "CircleTexture.hpp"
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include <cstdint>
+#include <iostream>
+#include <memory>
+
+Game::Game() : 
+	initialized_(false), 
+	running_(false), 
+	circle_texture_(nullptr)
+{
+	initialized_ = Initialize();
+
+	const SDL_Color color = { 0x00, 0x00, 0xff, 0xff };
+	const Point<NumType> center = { static_cast<NumType>(constants::screen_width / 2), static_cast<NumType>(constants::screen_height / 2) };
+	const NumType radius = 100;
+	circle_texture_ = std::make_unique<CircleTexture<NumType>>(renderer_, center, radius, color);
+}
+
+Game::~Game()
+{
+	Finalize();
+}
+
+bool Game::Initialize()
+{
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		printf("SDL could not be initialized! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "0"))
+	{
+		printf("%s\n", "Warning: Texture filtering is not enabled!");
+	}
+
+	window_ = SDL_CreateWindow(constants::game_title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, constants::screen_width, constants::screen_height, SDL_WINDOW_SHOWN);
+
+	if (window_ == nullptr)
+	{
+		printf("Window could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	renderer_ = SDL_CreateRenderer(window_, -1, SDL_RENDERER_ACCELERATED);
+
+	if (renderer_ == nullptr)
+	{
+		printf("Renderer could not be created! SDL Error: %s\n", SDL_GetError());
+		return false;
+	}
+
+	constexpr int img_flags = IMG_INIT_PNG;
+
+	if (!(IMG_Init(img_flags) & img_flags))
+	{
+		printf("SDL_image could not be initialized! SDL_image Error: %s\n", IMG_GetError());
+		return false;
+	}
+
+	return true;
+}
+
+void Game::Finalize()
+{
+	SDL_DestroyWindow(window_);
+	window_ = nullptr;
+	
+	SDL_DestroyRenderer(renderer_);
+	renderer_ = nullptr;
+
+	SDL_Quit();
+	IMG_Quit();
+}
+
+void Game::Run()
+{
+	if (!initialized_)
+	{
+		printf("%s\n", "Game has not been initialized!");
+		return;
+	}
+
+	running_ = true;
+
+	constexpr double ms = 1.0 / 60.0;
+	std::uint64_t last_time = SDL_GetPerformanceCounter();
+	long double delta = 0.0;
+
+	double timer = SDL_GetTicks();
+
+	int frames = 0;
+	int ticks = 0;
+
+	while (running_)
+	{
+		const std::uint64_t now = SDL_GetPerformanceCounter();
+		const long double elapsed = static_cast<long double>(now - last_time) / static_cast<long double>(SDL_GetPerformanceFrequency());
+
+		last_time = now;
+		delta += elapsed;
+
+		HandleEvents();
+
+		while (delta >= ms)
+		{
+			Tick();
+			delta -= ms;
+			++ticks;
+		}
+
+		//printf("%Lf\n", delta / ms);
+		Render();
+		++frames;
+
+		if (SDL_GetTicks() - timer > 1000.0)
+		{
+			timer += 1000.0;
+			// printf("Frames: %d, Ticks: %d\n", frames, ticks);
+			frames = 0;
+			ticks = 0;
+		}
+	}
+}
+
+void Game::HandleEvents()
+{
+	SDL_Event e;
+
+	while (SDL_PollEvent(&e) != 0)
+	{
+		if (e.type == SDL_QUIT)
+		{
+			running_ = false;
+			return;
+		}
+		if (e.type == SDL_MOUSEBUTTONDOWN && e.button.button == SDL_BUTTON_LEFT)
+		{
+			//SDL_GetMouseState(&mouse_pos_.x, &mouse_pos_.y);
+		}
+	}
+}
+
+void Game::Tick()
+{
+	SDL_GetMouseState(&mouse_pos_.x, &mouse_pos_.y);
+	circle_texture_->MoveTo({ mouse_pos_.x, mouse_pos_.y });
+}
+
+void Game::Render()
+{
+	SDL_RenderSetViewport(renderer_, NULL);
+	SDL_SetRenderDrawColor(renderer_, 0xff, 0xff, 0xff, 0xff);
+	SDL_RenderClear(renderer_);
+
+	circle_texture_->Render();
+		
+	SDL_RenderPresent(renderer_);
+}
